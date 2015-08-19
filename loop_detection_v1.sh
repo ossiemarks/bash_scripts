d40559a705f6b0ab4e9b6ff6990fa581ecf3dcf0
ossiemarks <osmanmarks@icloud.com>
2015-08-19 11:31:05 +0100

    Create loop_detection_v1.sh
    No updates the file after each run

---
+++ loop_detection_v1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Detect loops in the istats hadoop post
+
+FILE="/tmp/current_iplayer_file"
+
+if [ -f "$FILE" ]
+then
+        job_type=`hadoop dfs -ls  hdfs://hadoopn.rep-back.lh.omg.bbc.co.uk/user/lisuser/iplayer_livestats/ | tail -n 1 |awk {'print $8'} | cut -d "/" -f 4`
+        Old_File=$(cat /tmp/current_iplayer_file)
+        Current_File=`egrep '\-put /var/istats/lisuser/var/poller/iplayer_livestats' /var/log/istats/lisuser/lisv2-app.log  | awk {'print $10'}| tail -n 1`
+        #DFS_FILE=`hadoop dfs -ls  hdfs://hadoopn.rep-back.lh.omg.bbc.co.uk/user/lisuser/iplayer_livestats/ | tail -n 1 |awk {'print $8'}`
+        #if [ "$Current_File" == "$Old_File" && -z "$DFS_FILE" ]
+         if [ "$Current_File" == "$Old_File" ]
+
+                then
+                         mail -s "Looping Iplayer Istats" < /tmp/current_iplayer_file osman.marks.ext@bbc.co.uk tom.collins@bbc.co.uk craig.phillips@bbc.co.uk david.hodgkinson.ext@bbc.co.uk
+                else
+                echo "All Looks Good"
+                echo "$Current_File" > /tmp/current_iplayer_file
+                exit 0
+                fi
+
+else
+        echo " No Record of previous run"
+        egrep '\-put|Executing /usr/lib/hadoop/bin/hadoop fs -put /var/istats/lisuser/var/poller/iplayer_livestats' /var/log/istats/lisuser/lisv2-app.log  | awk {'print $10'} | tail -n 1 > /tmp/current_iplayer_file
+        exit
+fi
