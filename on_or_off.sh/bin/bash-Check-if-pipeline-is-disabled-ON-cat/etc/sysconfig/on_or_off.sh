13d2d258cfa12dd5ed456cdb5edf6f4e53c91b83
ossiemarks <osmanmarks@icloud.com>
2015-09-08 14:21:35 +0100

    Update on_or_off.sh

--- on_or_off.sh/bin/bash-Check-if-pipeline-is-disabled-ON-cat/etc/sysconfig/on_or_off.sh
+++ on_or_off.sh/bin/bash-Check-if-pipeline-is-disabled-ON-cat/etc/sysconfig/on_or_off.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Check if pipeline is disabled
-
 ON=`cat /etc/sysconfig/bbc-istats-controller | head -n9 | tail -n 1 | cut -f 2 -d '"' `
-
+host=`hostname -f`
 if [ $ON -gt 0 ]
 then
 echo "PIPELINE IS ON"
 else
-mail -s "PIPELINE IS TURNED OFF" osman.marks.ext@bbc.co.uk < `date`
+mail -s "PIPELINE IS TURNED OFF ${host} " osman.marks.ext@bbc.co.uk tom.collins@bbc.co.uk craig.phillips@bbc.co.uk david.hodgkinson.ext@bbc.co.uk iStatsAVSupport@bbc.co.uk < /tmp/problems
 fi
